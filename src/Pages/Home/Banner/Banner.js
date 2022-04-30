a2cf326cdf294506aacc77073773a02358ba6463
faisalkn-71 <csust2021@gmail.com>
2022-04-30 17:04:48 +0600

    banner-done

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <img src="https://i.ibb.co/GkCSPvP/banner.webp" alt="" />
+            
+        </div>
+    );
+};
+
+export default Banner;
+
+
---
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h4>The page you are looking for is not found!!!</h4>
+            <br />
+            <br />
+            <h1>404</h1>
+        </div>
+    );
+};
+
+export default NotFound;
