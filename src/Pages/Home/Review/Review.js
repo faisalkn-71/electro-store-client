69062c975afc74fa828124623f4e25380a1db2ec
faisalkn-71 <csust2021@gmail.com>
2022-04-30 22:32:20 +0600

    reviews-responsive-done

--- src/Pages/Home/Review/Review.js
+++ src/Pages/Home/Review/Review.js
@@ -4,7 +4,7 @@ const Review = (props) => {
     const {name, image, feedback} = props.review;
     
     return (
-        <div>
+        <div className='border border-dark rounded p-3'>
             <img width="50px" height="50px"  src={image} alt="" />
             <h5>{name}</h5>
             <p className='text-start'><small>{feedback}</small></p>
